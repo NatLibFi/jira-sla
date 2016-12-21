82ed646cc65fe29661bdcda03bcf712d4d456cd5
Timo Laine <timo.mz.laine@helsinki.fi>
2016-12-21 14:05:40 +0200

    Licensing information.

--- jira-sla.php
+++ jira-sla.php
@@ -1,6 +1,22 @@
-#!/usr/local/bin/php
-
 <?php
+
+/*
+ *  This file is part of Jira-SLA.
+ *
+ *  Jira-SLA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Jira-SLA is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Jira-SLA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 use chobie\Jira\Api;
 use chobie\Jira\Api\Authentication\Basic;
